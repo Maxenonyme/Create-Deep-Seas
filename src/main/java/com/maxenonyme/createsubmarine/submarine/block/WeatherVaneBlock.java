c8e471ee8528f4c692f2f433b0c97fbc232efc28
daileta <evanthomasdailey@gmail.com>
2026-06-24 07:04:03 -0500

    Added weather vane block

---
+++ src/main/java/com/maxenonyme/createsubmarine/submarine/block/WeatherVaneBlock.java
@@ -0,0 +1,26 @@
+package com.maxenonyme.createsubmarine.submarine.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class WeatherVaneBlock extends Block {
+    private static final VoxelShape SHAPE = Block.box(1.0, 0.0, 0.0, 15.0, 16.0, 16.0);
+
+    public WeatherVaneBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+}
